4e7757deddc75e4581de34f5ba8c624d99f65dd9
gokul <gokuljayanjb99@outlook.com>
2024-01-15 16:06:22 +0530

    first commit

---
+++ app/src/main/java/com/example/memorygame/utils/Constants.kt
@@ -0,0 +1,19 @@
+package com.example.memorygame.utils
+
+import com.example.memorygame.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.face,
+    R.drawable.air,
+    R.drawable.vehicle,
+    R.drawable.android,
+    R.drawable.clock,
+    R.drawable.family,
+    R.drawable.flower,
+    R.drawable.snowflake,
+    R.drawable.numbers,
+    R.drawable.infinity,
+    R.drawable.heart,
+    R.drawable.threesixty,
+
+)
